6e3c80840f2b94c6f1f0f9097fa3b9cfcf60beda
she changlue <snakepointid@sina.cn>
2017-02-14 19:30:39 +0800

    update

--- word2vecMac/word2vecMac/main.cpp
+++ word2vecMac/word2vecMac/main.cpp
@@ -1,12 +1,24 @@
+#include <chrono>
 #include<iostream>
-#include<string>
-#include "vecComp.h"
-#include<vector>
-#include "snakeWord2Vec.h"
+#include <stdio.h>
+#include"word2vec.h"
 using namespace std;
-
 int main()
 {
-    SW2V test;
-    
+	SW2V myModel;
+	auto cstart = std::chrono::high_resolution_clock::now();
+	myModel.loadCorpus();
+	auto cend = std::chrono::high_resolution_clock::now();
+	printf("load vocab: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+
+	cstart = std::chrono::high_resolution_clock::now();
+	myModel.processCorpus();
+	cend = std::chrono::high_resolution_clock::now();
+	printf("build vocab: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+
+	cstart = std::chrono::high_resolution_clock::now();
+	myModel.trainCorpus();
+	cend = std::chrono::high_resolution_clock::now();
+	printf("train vocab: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+
 }
--- word2vecMac/word2vecMac/vecComp.h
+++ word2vecMac/word2vecMac/vecComp.h
@@ -1,4 +1,4 @@
-//
+﻿//
 //  vecComp.h
 //  word2vecMac
 //
@@ -8,40 +8,39 @@
 
 #ifndef vecComp_h
 #define vecComp_h
-#include<cmath>
 #include<vector>
 using std::vector;
 using Vector = vector<float>;
-template<class Vector1,class Vector2>
-inline float sDot(const Vector1 &x,const Vector2 &y)
+template<class Vector1, class Vector2>
+inline float sDot(const Vector1 &x, const Vector2 &y)
 {
-    int m = x.size();const float *xp = x.data(),*yp = y.data();
-    float sum=0.0;
-    while (m-->0)
-    {
-        sum+=(*xp++)*(*yp++);
-    }
-    return sum;
+	int m = x.size(); const float *xp = x.data(), *yp = y.data();
+	float sum = 0.0;
+	while (m-->0)
+	{
+		sum += (*xp++)*(*yp++);
+	}
+	return sum;
 }
-inline void sSaxpy( Vector &x,float g,const Vector &y)
+inline void sSaxpy(Vector &x, float g, const Vector &y)
 {
-    int m = x.size();float *xp = x.data();const float *yp = y.data();
-    while (m-->0)(*xp++)+=(*yp++)*g;  
+	int m = x.size(); float *xp = x.data(); const float *yp = y.data();
+	while (m-->0)(*xp++) += (*yp++)*g;
 }
 inline void sUnit(Vector &x)
 {
-    float norm = ::sqrt(sDot(x,x));
-    if(norm==0) return;
-    int m = x.size();float *xp = x.data(); 
-    while (m-->0)(*xp++)/=norm;
+	float norm = ::sqrt(sDot(x, x));
+	if (norm == 0) return;
+	int m = x.size(); float *xp = x.data();
+	while (m-->0)(*xp++) /= norm;
 
 }
 inline float sSum(const Vector &x)
 {
-    float sum=0.0;
-    int m = x.size();const float *xp = x.data(); 
-    while (m-->0)sum+=*xp++;
-    return sum;
+	float sum = 0.0;
+	int m = x.size(); const float *xp = x.data();
+	while (m-->0)sum += *xp++;
+	return sum;
 
 }
 #endif /* vecComp_h */
