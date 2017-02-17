dc64ca9c341d0f314a7e893709740c0ee2abc72a
she changlue <snakepointid@sina.cn>
2017-02-17 14:11:55 +0800

    update

--- word2vecPC/word2vecPC/main.cpp
+++ word2vecPC/word2vecPC/main.cpp
@@ -1,24 +1,108 @@
 #include <chrono>
 #include<iostream>
+#include<vector>
 #include <stdio.h>
 #include"word2vec.h"
 using namespace std;
+ 
+void initHyperPara(float &alpha, float & min_alpha, int &negSamples, int &window, int &wordDim, int &min_count, float &subsample, int &spanNum, float & maxExp);
 int main()
 {
-	SW2V myModel;
-	auto cstart = std::chrono::high_resolution_clock::now();
-	myModel.loadCorpus();
-	auto cend = std::chrono::high_resolution_clock::now();
-	printf("load vocab: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+	float alpha = 0.1;
+	float min_alpha = 0.001;
+	int negSamples = 10;
+	int window = 3;
+	int wordDim = 100;
+	int min_count = 1;
+	float subsample = 0.001;
+	int spanNum = 1000;
+	float maxExp = 6.0;
+	string mode,corpusFN, WVloadFN;
+	printf("please choose your mode:\n'train'--for training corpus\n'test'--for test word vec similar\n");
+	cin >> mode;
+	if (mode == "train")
+	{
+		initHyperPara(alpha, min_alpha, negSamples, window, wordDim, min_count, subsample, spanNum, maxExp);
+		SW2V myModel(alpha, min_alpha, negSamples, window , wordDim,min_count, subsample,spanNum, maxExp);
+		printf("please enter your corpus file name:");
+		cin >> corpusFN;
+		printf("\nplease enter your word vec save file name:");
+		cin >> WVloadFN;
 
-	cstart = std::chrono::high_resolution_clock::now();
-	myModel.processCorpus();
-	cend = std::chrono::high_resolution_clock::now();
-	printf("build vocab: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+		auto cstart = std::chrono::high_resolution_clock::now();
+		myModel.loadCorpus(corpusFN);
+		auto cend = std::chrono::high_resolution_clock::now();
+		printf("load corpus: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
 
-	//cstart = std::chrono::high_resolution_clock::now();
-	//myModel.trainCorpus();
-	//cend = std::chrono::high_resolution_clock::now();
-	//printf("train vocab: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+		cstart = std::chrono::high_resolution_clock::now();
+		myModel.processCorpus();
+		cend = std::chrono::high_resolution_clock::now();
+		printf("build vocab: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+		int loopt = 100;
+		while (loopt>0)
+		{
+			cstart = std::chrono::high_resolution_clock::now();
+			for (int i = 0; i < loopt; i++)myModel.trainCorpus();
+			cend = std::chrono::high_resolution_clock::now();
+			printf("train vocab: %.4f seconds per loop\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0/ loopt);
+			myModel.testWordSimilar();
+			printf("do you want to contitue training, enter your loop times:(0 be quit)\n");
+			cin >> loopt;
+		}
 
+
+		cstart = std::chrono::high_resolution_clock::now();
+		myModel.saveWord2vec(WVloadFN);
+		cend = std::chrono::high_resolution_clock::now();
+		printf("saveWord2vec: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+
+	}
+	if (mode == "test")
+	{
+		SW2V myModel(alpha, min_alpha, negSamples, window, wordDim, min_count, subsample, spanNum, maxExp);
+		printf("\nplease enter your word vec save file name:");
+		cin >> WVloadFN;
+		auto cstart = std::chrono::high_resolution_clock::now();
+		myModel.loadWord2vec(WVloadFN);
+		auto cend = std::chrono::high_resolution_clock::now();
+		printf("loadWord2vec: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+		myModel.testWordSimilar();
+
+	}
+}
+ 
+void initHyperPara(float & alpha, float & min_alpha, int & negSamples, int & window, int & wordDim, int & min_count, float & subsample, int & spanNum, float & maxExp)
+{
+	printf("the default parameters' values are:\n------------------------------------\n");
+	printf("#1 alpha     : %.4f---control the model learning rate \n", alpha);
+	printf("#2 min_alpha : %.4f---control the model smallest learning rate,because the learning rate decay in Adam \n", min_alpha);
+	printf("#3 negSamples: %lu---the negtive sampling numbers \n", negSamples);
+	printf("#4 window    : %lu---the context window size \n", window);
+	printf("#5 wordDim   : %lu---the word vec dimemsion \n", wordDim);
+	printf("#6 min_count : %lu---determine which word is infrequency word \n", min_count);
+	printf("#7 subsample : %.4f---control the subsampling rate \n", subsample);
+	printf("#8 spanNum   : %lu---the sigmoid projection table size,recommend keep default \n", spanNum);
+	printf("#9 maxExp    : %.4f---the sigmoid projection table max and min zone,recommend keep default \n", maxExp);
+	printf("choose the parameter number(eta. alpha is 1) you want to change(0 to quit):");
+	int c;
+	cin >> c;
+	while (c != 0)
+	{
+		switch (c)
+		{
+		case 0: break;
+		case 1: printf("\nalpha   :"); cin >> alpha; break;
+		case 2: printf("\nmin_alpha   :"); cin >> min_alpha; break;
+		case 3: printf("\nnegSamples   :"); cin >> negSamples; break;
+		case 4: printf("\nwindow   :"); cin >> window; break;
+		case 5: printf("\nwordDim   :"); cin >> wordDim; break;
+		case 6: printf("\nmin_count   :"); cin >> min_count; break;
+		case 7: printf("\nsubsample   :"); cin >> subsample; break;
+		case 8: printf("\nspanNum   :"); cin >> spanNum; break;
+		case 9: printf("\nmaxExp   :"); cin >> maxExp; break;
+		default:printf("no such parameter,please choose again"); break; 		
+		}
+		printf("choose the parameter you want to change(0 to quit):");
+		cin >> c;
+	}
 }
--- word2vecPC/word2vecPC/word2vec.h
+++ word2vecPC/word2vecPC/word2vec.h
@@ -8,6 +8,7 @@
 #include <vector>
 #include <list>
 #include <string>
+#include<iostream>
 #include <unordered_map>
 #include <tuple>
 #include <algorithm>
@@ -15,6 +16,7 @@
 #include <random>
 #include <memory>
 #include <fstream>
+ 
 #include <sstream>
 #include<map>
 #include <chrono>
@@ -39,29 +41,36 @@ struct SW2V
 	vector<string>   indexs2tokens_;
 	vector<int>		 tokensCount_;
 	vector<std::pair<string, int>> vocab_;
-	vector<Vector>   inWeight;
-	vector<Vector>   outWeight;
-	Vector           gradHolder;
+	vector<Vector>   inWeight_;
+	vector<Vector>   inWeightNorm_;
+	vector<Vector>   outWeight_;
+	Vector           gradHolder_;
+	int              total_words_=0;
 	//speed up table
-	vector<int>unigram;
-	vector<float>sigTable;
-	int spanNum = 1000;
-	float maxExp = 6.0;
-	//controler
-	bool phrase_ = true;
-	int min_token_count_ = 0;
-	float alpha_ = 0.025;
-	float min_alpha_ = 0.0001;
-	float subsample_ = 0;
-
-	int negSamples_ = 5;
-	int contextWindow = 5;
-	int total_words;
-	int wordDim = 100;
+	vector<int>      unigram;
+	vector<float>    sigTable;
+	//controler	
+	float alpha_;
+	float min_alpha_;
+	int negSamples_;
+	int window_;
+	int wordDim_;
+	int min_count_;
+	float subsample_;
+	int spanNum_;
+	float maxExp_;
+	//construct funcs
+	SW2V(float alpha = 0.1, float min_alpha = 0.001, int negSamples = 10, int window = 3, int wordDim = 100,
+		int min_count = 1, float subsample = 0.001, int spanNum = 1000, float maxExp = 6.0) 
+		:alpha_(alpha), min_alpha_(min_alpha), negSamples_(negSamples), window_(window), wordDim_(wordDim),
+		min_count_(min_count), subsample_(subsample), spanNum_(spanNum), maxExp_(maxExp){};
+	//functions
+	//processCorpus method which can get a vocabulary staticstic and transfer the sentences' tokens to index
 	int processCorpus()
 	{
-		auto cstart = std::chrono::high_resolution_clock::now();
+		//last_token to hold the previews token
 		string last_token;
+		//tokensCount which temperary count the tokens numbers
 		std::unordered_map<string, int>tokensCount;
 		//count the token numbers
 		for (auto &sentence : sentences)
@@ -69,93 +78,20 @@ struct SW2V
 			if (sentence->tokens.empty())continue;
 			for (auto &token : sentence->tokens)
 			{
+				total_words_ += 1;
 				tokensCount[token] += 1;
-				if (phrase_)
-				{
-					if (!last_token.empty()) { tokensCount[last_token + "_" + token] += 1; }
-				}
 				last_token = token;
 			}
 		}
-		total_words = std::accumulate(tokensCount.begin(), tokensCount.begin(), 0, [](int x, const std::pair<string, int>&v) {return x + v.second; });
-		//select phrase base simple statistic
-		 
-		if (phrase_)
-		{
-			std::unordered_map<string, int>phraseCount;
-			for (auto &sentence : sentences)
-			{
-				vector<string>phraseTokens;
-				if (sentence->tokens.empty())continue;
-				for (auto &token : sentence->tokens)
-				{
-					if (!last_token.empty())
-					{
-						string phrase = last_token + "_" + token;
-						int pa = tokensCount[last_token], pb = tokensCount[token], pab = tokensCount[phrase];
-						float score = 0.0;
-						if (pab > min_token_count_)
-						{
-							score = (pab - min_token_count_) / ((float)pa*pb)*total_words;
-							if (score > 100)
-							{
-								phraseCount[phrase] += 1;
-								token.clear();
-								phraseTokens.push_back(move(phrase));
-							}
-							else if (pa>min_token_count_)
-							{
-								phraseCount[last_token] += 1;
-								phraseTokens.push_back(move(last_token));
-							}
-						}
-					}
-					last_token = token;
-					if (!last_token.empty() && tokensCount[last_token]>min_token_count_)
-					{
-						phraseCount[last_token] += 1;
-						phraseTokens.push_back(move(last_token));
-					}
-				}
-				int len = phraseTokens.size();
-				sentence->tokens.swap(phraseTokens);
-				sentence->indexs.reserve(len);
-			}
-			tokensCount.swap(phraseCount);
-		}
-		auto cend = std::chrono::high_resolution_clock::now();
-		printf("process tokens: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
-
-		total_words = std::accumulate(tokensCount.begin(), tokensCount.end(), 0, [](int x, const std::pair<string, int>& v) { return x + v.second; });
-		std::cout << "total words:" << total_words << std::endl;
-		if (total_words < 1)return -1;
-		int n_words = tokensCount.size();
-		cstart = std::chrono::high_resolution_clock::now();
-		//initial weights
+		printf("the corpus have total %lu [words] with %lu distinct [tokens]\n", total_words_, tokensCount.size());
+		if (total_words_ < 1)return -1;
+		//initial members 1st
 		std::default_random_engine eng(::time(NULL));
 		std::uniform_real_distribution<float> rng(0.0, 1.0);
-		inWeight.resize(n_words);
-		outWeight.resize(n_words);
+		int n_words = tokensCount.size();
+		vocab_.reserve(n_words);
 		indexs2tokens_.reserve(n_words);
 		tokensCount_.reserve(n_words);
-		vocab_.reserve(n_words);
-		gradHolder.resize(wordDim);
-		for (auto&w : inWeight)
-		{
-			w.resize(wordDim);
-			for (auto&v : w)
-			{
-				v = (rng(eng) - 0.5) / wordDim;
-			}
-		}
-		for (auto&w : outWeight)
-		{
-			w.resize(wordDim);
-		}
-		cend = std::chrono::high_resolution_clock::now();
-		printf("initial weights: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
-		cstart = std::chrono::high_resolution_clock::now();
-		//initial members
 		for (auto&pr : tokensCount)
 		{
 			vocab_.push_back(move(make_pair(pr.first, pr.second)));
@@ -163,99 +99,207 @@ struct SW2V
 		auto comp = [](std::pair<string, int>&v1, std::pair<string, int>&v2) {return v1.second > v2.second; };
 		std::sort(vocab_.begin(), vocab_.end(), comp);
 		int index = 0;
+		int infreqWordCount = 0;
 		for (auto&pr : vocab_)
-		{
-			tokens2indexs_[pr.first] = index++;
-			indexs2tokens_.push_back(move(pr.first));
-			tokensCount_.push_back(pr.second);
+		{		
+			tokens2indexs_[pr.first] = index;
+			if (pr.second > min_count_) { index++; indexs2tokens_.push_back(move(pr.first)); tokensCount_.push_back(pr.second);}
+			else { infreqWordCount += pr.second; }
+			if(infreqWordCount>0){ indexs2tokens_.push_back("infreqWord"); tokensCount_.push_back(infreqWordCount); }
 		}
+		//shape those member
+		n_words = indexs2tokens_.size();
+		inWeight_.resize(n_words);
+		outWeight_.resize(n_words);
+		gradHolder_.resize(wordDim_);	
+		//inital weights
+		for (auto&w : inWeight_)
+		{
+			w.resize(wordDim_);
+			for (auto&v : w)
+			{
+				v = (rng(eng) - 0.5) / wordDim_;
+			}
+		}
+		for (auto&w : outWeight_)w.resize(wordDim_);
+ 
+		//transfer the sentences
 		for (auto&sentence : sentences)
 		{
-			if (sentence->tokens.size()<3)continue;
+			if (sentence->tokens.empty())continue;
 			for (auto&token : sentence->tokens)
 			{
 				sentence->indexs.push_back(tokens2indexs_[token]);
 			}
-		}
-		cend = std::chrono::high_resolution_clock::now();
-		printf("initial members: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+		}	
 		//negtive sample table
-		cstart = std::chrono::high_resolution_clock::now();
-		unigram.resize(std::min((int)1e8, total_words));
-		int uniSize = unigram.size();
+		unigram.resize(std::min((int)1e8, total_words_));
+		int   unigramSize = unigram.size();
 		float power = 0.75;
 		float sum = std::accumulate(tokensCount.begin(), tokensCount.end(), 0, [&power](float x, const std::pair<string, int>& v) { return x + ::pow(v.second,power); });
 		float d1 = ::pow(tokensCount_[0], power) / sum;
 		int idx = 0;
-		for (int a = 0; a < uniSize; a++)
+		for (int a = 0; a < unigramSize; a++)
 		{
-			if (a / uniSize > d1) { idx++; d1 += ::pow(tokensCount_[idx], power) / sum;}
+			if (a / unigramSize > d1) { idx++; d1 += ::pow(tokensCount_[idx], power) / sum;}
 			unigram[a] = idx;
 			if (idx > n_words - 1)idx = n_words - 1;
 		}
-		printf("unigram size:%lu;with sum:%.4f\n", unigram.size(), sum);
-		cend = std::chrono::high_resolution_clock::now();
-		printf("table speed up: %.4f seconds\n", std::chrono::duration_cast<std::chrono::microseconds>(cend - cstart).count() / 1000000.0);
+		printf("unigram size:%lu;with power sum:%.4f\n", unigram.size(), sum);
 		//sigmoid value table
-		sigTable.resize(spanNum);
-		for (int i = 0; i < spanNum; i++)
+		sigTable.resize(spanNum_);
+		for (int i = 0; i < spanNum_; i++)
 		{
-			float f = ::exp(i / spanNum * 2 - 1);
+			float f = ::exp(i / spanNum_ * 2 - 1);
 			sigTable[i] = f / (f + 1);;			
 		}
-		std::cout << "haha to the final\n";
 		return 1;
 	}
 	void trainCorpus()
 	{
 		std::default_random_engine eng(::time(NULL));
 		std::uniform_real_distribution<float> rng(0.0, 1.0);
-
-		int currentWordCount = 0;
-		int trainedSentences = 0;
+		static int currentWordCount = 0;
+		static unsigned int trainedSentences = 0;
+		static unsigned int TrainedSentences = 0;
+		float alpha;
 #pragma omp parallel for
 		for (int i = 0; i<sentences.size(); i++)
 		{
 			int trainWords = 0;
 			auto &sentence = sentences[i];
-			if (sentence->indexs.empty())continue;
-			
-			float alpha = std::max(min_alpha_, alpha_*(1 - currentWordCount / total_words));
+			if (sentence->indexs.size()<3)continue;
+			if (currentWordCount > total_words_)currentWordCount = total_words_;
+			alpha = std::max(min_alpha_, alpha_*(float)(1 - currentWordCount / total_words_));
 			//subsample
 			vector<int>tmpSentence;
 			for (auto &idx : sentence->indexs)
 			{
-				float rnd = (::sqrt(tokensCount_[idx] / total_words / subsample_) + 1)*(subsample_*total_words / tokensCount_[idx]);
-				if (rnd > rng(eng)| subsample_==0)tmpSentence.push_back(idx);
-			};
+				if (subsample_ > 0)
+				{
+					float rnd = (::sqrt(tokensCount_[idx] / total_words_ / subsample_) + 1)*(subsample_*total_words_ / tokensCount_[idx]);
+					if (rnd > rng(eng))tmpSentence.push_back(idx);
+				}
+				else
+				{
+					tmpSentence.push_back(idx);
+				}			
+			}
 			if (tmpSentence.size() > 1) {
 				trainedSentences += 1; trainWords = trainSentence(tmpSentence, alpha);
 			}
 #pragma omp atomic
-			currentWordCount += trainWords;
-		}
-		std::cout << "prepare trained sentences:" << trainedSentences << "\n";
-		std::cout << "total trained words:" << currentWordCount << "\n";
+			currentWordCount +=  trainWords;
+		}		
+		if (trainedSentences > 1000) { trainedSentences -= 1000; TrainedSentences += 1; }
+		printf("total trained sentences: %luk|alpha: %.4f\n", TrainedSentences, alpha);
+
 	}
-	void loadCorpus()
+	void testWordSimilar()
 	{
-		string fn("test.txt");
+		inWeightNorm_ = inWeight_;
+		for (auto&w : inWeightNorm_)sUnit(w);
+		string token;
+		vector<std::pair<int, float>>dist;
+		dist.reserve(indexs2tokens_.size());
+		while(1)
+		{
+			printf("please enter the word you want to test('#quit' to quit):");
+			std::cin >> token;
+			if (token=="#quit")break;
+			printf("the word you want to test is' %s '\n",token.c_str());
+			lowerTokens(token);
+			auto it = tokens2indexs_.find(token.c_str());
+			if (it == tokens2indexs_.end()) { printf("word is not exist!!!\n"); continue; }
+			Vector &targVec = inWeightNorm_[it->second];
+		 
+			for (int idx=0;idx<inWeightNorm_.size();idx++)
+			{
+				dist.push_back(std::move(std::make_pair(idx, sDot(targVec, inWeightNorm_[idx]))));
+			}
+			auto comp = [](std::pair<int, float>&v1, std::pair<int, float>&v2) {return v1.second > v2.second; };
+			std::sort(dist.begin(), dist.end(), comp);
+			for (int i = 0; i < 11; i++) {
+				if (it->second == dist[i].first)continue;
+				printf("%lu most similar: %s \n", i, indexs2tokens_[dist[i].first].c_str());
+			}
+			printf("---------------------------------------------------------------------------\n");
+			dist.clear();
+			token.clear();
+		}
+		
+	}
+	string& lowerTokens(string&token)
+	{
+		transform(token.begin(), token.end(), token.begin(), tolower);
+		return token;
+	}
+	string& upperTokens(string&token)
+	{
+		transform(token.begin(), token.end(), token.begin(), toupper);
+		return token;
+	}
+	void saveWord2vec(string&fn)
+	{
+		std::ofstream fin;
+		fin.open(fn);
+		fin << inWeight_.size() << " " << inWeight_[0].size() << "\n";
+		for (int idx = 0; idx < indexs2tokens_.size(); idx++)
+		{
+			fin << indexs2tokens_[idx] << " ";
+			for (auto&w : inWeight_[idx]) { fin << w << " "; }
+			fin << "\n";
+		}
+		fin.close();
+	}
+	int loadWord2vec(string&fn)
+	{
+		inWeight_.clear();
 		std::ifstream fin;
-	/*	std::cout << "please enter your raw corpus file name:";
-		std::cin >> fn;*/
+		string line;
+		string token;
+		fin.open(fn);
+		if (!getline(fin, line)) { std::cout << "there have no data\n"; return -1; }
+		int n_words = 0, wordDim = 0;	
+		std::stringstream(line) >> n_words >> wordDim;
+		inWeight_.resize(n_words);
+		for (auto&w : inWeight_)w.resize(wordDim);
+		int idx = 0;
+		float wt;
+		while (getline(fin, line))
+		{
+			std::stringstream ipt(line);
+			ipt >> token;
+			
+			int widx = 0;
+			while (ipt >> wt)
+			{
+				inWeight_[idx][widx++] = wt;
+			}
+			tokens2indexs_[token] = idx++;
+			indexs2tokens_.push_back(move(token));
+		}
+		fin.close();
+	}
+ 
+	void loadCorpus(string&fn)
+	{
+		std::ifstream fin;
 		SentenceP sentence(new Sentence);
 		string line;
 		string token;
 		fin.open(fn);
 		while (std::getline(fin, line))
 		{
+			//if (sentences.size() > 1000)break;
 			if (line.empty())continue;
 			std::stringstream str(line);
 			while (str >> token)
 			{
-				sentence->tokens.push_back(move(token));
+				sentence->tokens.push_back(move(lowerTokens(token)));
+				//sentence->tokens.push_back(move(token));
 			}
-			sentences.push_back(std::move(sentence));
+			sentences.push_back(move(sentence));
 			sentence.reset(new Sentence);
 		}
 		fin.close();
@@ -268,45 +312,44 @@ private:
 		for (int targCur = 0; targCur < sentLens; targCur++)
 		{
 			int targTokenIdex = sentence[targCur];
-			int reduced_window = rand() % contextWindow;
+			int reduced_window = rand() % window_;
 			int lowCur = std::max(0, targCur - reduced_window - 1);
 			int upCur = std::min(sentLens, targCur + reduced_window + 1);
-			Vector&targWeight = inWeight[targTokenIdex];
+			Vector&targWeight = inWeight_[targTokenIdex];
 			for (; lowCur<upCur; lowCur++)
 			{
-				std::fill(gradHolder.begin(), gradHolder.end(), 0);
+				std::fill(gradHolder_.begin(), gradHolder_.end(), 0.0);
 				if (lowCur == targCur)continue;
 				int contTokenIdex = sentence[lowCur];
-				Vector&contWeight = inWeight[contTokenIdex];
+				Vector&contWeight = outWeight_[contTokenIdex];
 				float f = sDot(targWeight, contWeight);
-				if (f <= -maxExp || f >= maxExp)
+				if (f <= -maxExp_ || f >= maxExp_)
 					continue;
-				int fi = int((f + maxExp) * (spanNum / maxExp / 2.0));
+				int fi = int((f + maxExp_) * (spanNum_ / maxExp_ / 2.0));
 				f = sigTable[fi];
 				float g = (1 - f) * alpha;
 				sSaxpy(contWeight, g, targWeight);
-				sSaxpy(gradHolder, g, contWeight);
+				sSaxpy(gradHolder_, g, contWeight);
 				for (int neg = 0; neg < negSamples_; neg++)
 				{
 					int negTokenIdex = unigram[rand() % unigram.size()];
 					if (negTokenIdex == targTokenIdex || negTokenIdex == contTokenIdex)continue;
-					Vector&contWeight = inWeight[negTokenIdex];
-					float f = sDot(targWeight, contWeight);
-					if (f <= -maxExp || f >= maxExp)
+					Vector&negWeight = outWeight_[negTokenIdex];
+					float f = sDot(targWeight, negWeight);
+					if (f <= -maxExp_ || f >= maxExp_)
 						continue;
-					int fi = int((f + maxExp) * (spanNum / maxExp / 2.0));
+					int fi = int((f + maxExp_) * (spanNum_ / maxExp_ / 2.0));
 					f = sigTable[fi];
 					float g = (0 - f) * alpha;
-					sSaxpy(contWeight, g, targWeight);
-					sSaxpy(gradHolder, g, contWeight);
+					sSaxpy(negWeight, g, targWeight);
+					sSaxpy(gradHolder_, g, negWeight);
 				}
-				sSaxpy(targWeight, 1.0, gradHolder);
-				 
+				sSaxpy(targWeight, 1.0, gradHolder_);	 
 			}
 		}
 		return sentLens;
 	}
-
+	
 };
 
 #endif /* snakeWord2Vec_h */
