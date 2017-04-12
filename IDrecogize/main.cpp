9bb422b86fa006550bd87e1d49d3e81bac71642d
she changlue <snakepointid@sina.cn>
2017-04-12 19:55:10 +0800

    update

--- IDrecogize/main.cpp
+++ IDrecogize/main.cpp
@@ -7,83 +7,86 @@
 #include <cstring>
 #include<sstream>
 using namespace std;
+string & trim(string &s, char p);
 string & trim(string &s);
 int main()
 {
-	ifstream fin("director.txt");
 	//list all of the images
-	string tessDir;
-	string inputImgDir;
-	getline(fin, tessDir);
-	getline(fin, inputImgDir);
-	fin.close();
-	if (tessDir.empty() | inputImgDir.empty()) { printf("can not find the director.txt \n"); char c; cin >> c; return 1; }
-	string outDir = inputImgDir+"saved\\";
+	string inputImgDir = "clean_pictures/";
 	char cmdstr[256];
-	
-	sprintf_s(cmdstr, "cd /d \"%s\" & dir/on/b>list.txt", inputImgDir.data());
+	sprintf_s(cmdstr, "python rotate_image.pyc");
 	system(cmdstr);
-	sprintf_s(cmdstr, "MD %ssaved", inputImgDir.data());
+	sprintf_s(cmdstr, "MD saved");
+	system(cmdstr);
+	sprintf_s(cmdstr, "cd /d \"%s\" & dir/on/b>../saved/picture_name_list.txt", inputImgDir.data());
+
 	system(cmdstr);
 	//get the image file name
 	vector<string>imgfns;
-	fin.open(inputImgDir+"list.txt");
-	string imgfn;
-	while (getline(fin, imgfn))
-	{
-		if (imgfn == "list.txt")continue;
-		imgfns.push_back(move(imgfn));
-	}
-	fin.close();
-	//image preprocess
-
-
+	ifstream finpc;
+	ifstream fin;
 	ofstream fout;
-	fout.open(outDir+"PeopleInfo.txt");
-	fout << "name,address,id\n";
-	for (auto&fn : imgfns)
+	fout.open("saved/PeopleInfo.txt");
+	fout << "contract_no,name,address,id\n";
+	finpc.open("saved/picture_name_list.txt");	
+	string fn;
+	while (getline(finpc, fn))
 	{
 		//preprocess the image eta. sharpen ,resize,crop the image
-		sprintf_s(cmdstr, "magick %s%s -sharpen 2 -resize 400x400 -crop 200x60+70+30 -resize 400x400 -sharpen 10 %sname.jpg", inputImgDir.data(), fn.data(), outDir.data());
+		sprintf_s(cmdstr, "magick clean_pictures/%s -sharpen 2 -resize 400x400 -crop 200x60+60+30 -resize 400x400 -sharpen 10 saved/name.png", fn.data());
 		system(cmdstr);
-		sprintf_s(cmdstr, "magick %s%s -sharpen 2 -resize 400x400 -crop 200x80+70+160 -resize 400x400 -sharpen 10 %saddress.jpg", inputImgDir.data(), fn.data(), outDir.data());
+		sprintf_s(cmdstr, "magick clean_pictures/%s -sharpen 2 -resize 400x400 -crop 200x80+60+160 -resize 400x400 -sharpen 10 saved/address.png", fn.data());
 		system(cmdstr);
-		sprintf_s(cmdstr, "magick %s%s -sharpen 2 -resize 400x400 -crop 260x50+130+270 -resize 400x400 -sharpen 10 %sid.jpg", inputImgDir.data(), fn.data(), outDir.data());
+		sprintf_s(cmdstr, "magick clean_pictures/%s -sharpen 2 -resize 400x400 -crop 260x60+120+270 -resize 400x400 -sharpen 10 saved/id.png", fn.data());
 		system(cmdstr);
 		//begin to regonize the info
-		sprintf_s(cmdstr, "cd /d \"%s\" & tesseract %sname.jpg %sname -l chi ", tessDir.data(), outDir.data(), outDir.data());
+		sprintf_s(cmdstr, "tesseract saved/name.png saved/name -l chi ");
 		system(cmdstr);
-		sprintf_s(cmdstr, "cd /d \"%s\" & tesseract %saddress.jpg %saddress -l chi ", tessDir.data(), outDir.data(), outDir.data());
+		sprintf_s(cmdstr, "tesseract saved/address.png saved/address -l chi ");
 		system(cmdstr);
-		sprintf_s(cmdstr, "cd /d \"%s\" & tesseract %sid.jpg %sid -l chi ", tessDir.data(), outDir.data(), outDir.data());
+		sprintf_s(cmdstr, "tesseract saved/id.png saved/id -l chi ");
 		system(cmdstr);
 		//read data
-		fin.open(outDir + "name.txt");
+		fin.open("saved/name.txt");
 		string name;
 		getline(fin, name);
 		fin.close();
-		fin.open(outDir + "id.txt");
+		fin.open("saved/id.txt");
 		string id;
 		getline(fin, id);
 		fin.close();
-		fin.open(outDir + "address.txt");
+		fin.open("saved/address.txt");
 		string address;
 		string addre;
 		while (getline(fin, addre))
 		{
-			if(addre.empty())break;
+			if (addre.empty())break;
 			address += addre;
 		}
+ 
 		fin.close();
-	 
-		if (name.empty())name = "NULL";
-		if (address.empty())address = "NULL";
-		if (id.empty())id = "NULL";
-		fout << trim(name) << "," << trim(address) << "," << trim(id) << "\n";
+
+		if (name.empty())name = " ";
+		if (address.empty())address = " ";
+		if (id.empty())id = " ";
+		fout << trim(fn, '.') <<"," << trim(name) << "," << trim(address) << "," << trim(id) << "\n";
 	}
 	fout.close();
+	finpc.close();
 }
 
+string & trim(string &s, char p)
+{
+	int index = 0;
+	if (!s.empty())
+	{
+		while ((index = s.find(p, index)) != string::npos)
+		{
+			s.erase(index, 10);
+		}
+	}
+	return s;
+}
 string & trim(string &s)
 {
 	int index = 0;
@@ -97,4 +100,3 @@ string & trim(string &s)
 	return s;
 }
 
- 
